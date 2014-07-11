08bf62bd76e627dfe1741f3051365c472642b233
Barry Gu <barry.gu@ftid.cn>
2014-07-11 18:29:55 +0800

    clean script

--- mgetv.sh
+++ mgetv.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-ipaddr=`echo $SSH_CLIENT | awk '{print $1}'`
-proxy="--socks5 $ipaddr:7070"
+ipaddr=`echo $SSH_CLIENT | sed "s/ .*//"`
+#proxy="--socks5 $ipaddr:7070"
+proxy=
 
 function getv() {
 	local key=$1
-	local count=0
+	local retry=10
 
 	local url="http://gaovideo.com/media/player/jwconfig.php?vkey=$key"
 	local ref="http://gaovideo.com/video/$key/"
@@ -14,36 +15,20 @@ function getv() {
 	cmd="curl $proxy \"$url\" -e \"$ref\""
 	echo $cmd
 
-	while [ $count -lt 10 ]; do
-   		#echo curl "$url" -e "$ref"
-   		#file=`curl "$url" -e "$ref" | sed -n -e "$expr"`
-   		#contents=`eval $cmd`
-   		#echo $contents
-   		file=`eval $cmd | sed -n -e "$exp"`
-		#file=`cat $contents | sed -n -e "$exp"`
-   		echo $file
-   		[ -n "$file" ] && break
-   		((count++))
-   		sleep 30
-   		#break
+	while [ $retry -gt 0 ]; do
+		file=`eval $cmd | sed -n -e "$exp"`
+		echo $file
+		[ -n "$file" ] && break;
+		((retry--))
+		sleep 5
 	done
 
 	[ -z "$file" ] && return
-	count=0
-	while [ $count -lt 10 ]; do
-		[ -f "$file" ] && rm -f $file
-		#wget -b "$file"
-		curl $proxy -O $file
-		[ $? -eq 0 ] && break
-		((count++))
-		sleep 30
-	done
+	curl $proxy -O $file
 }
 
-#echo param count: $#
 while [ $# -ge 1 ]
 do
-#	echo param: $1
 	getv $1
 	shift
 done
