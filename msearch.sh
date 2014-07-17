fd2673118e79b4c3007abe51a25eb3edc4174b8e
Barry Gu <barry.gu@ftid.cn>
2014-07-17 11:13:55 +0800

    fix wrong result file name

--- msearch.sh
+++ msearch.sh
@@ -7,7 +7,7 @@ query_result=$key"_results.html"
 keylist=$key"_lists.txt"
 
 [ -f $query_result ] || wget -O $query_result "$query_url$key"
-[ -f $keylist ] || grep "href=\"\/video[^>]\+><img" results.html | sed -ne "s/.*\/video\/\([0-9a-f]\+\)\/.*/\1/p" > $keylist
+[ -f $keylist ] || grep "href=\"\/video[^>]\+><img" $query_result | sed -ne "s/.*\/video\/\([0-9a-f]\+\)\/.*/\1/p" > $keylist
 list=`cat $keylist`
 
 #echo $list
