1efb39888b8048933f50e825d8a5403f89edcca6
Barry Gu <barry.gu@ftid.cn>
2014-07-27 08:59:09 +0800

    debounds for vnames

--- vnames.sh
+++ vnames.sh
@@ -20,22 +20,48 @@ get_vpage $pg_cur
 rm -f $target
 
 for vnum in ${numbs[@]}; do
+
+	step=0
+	step_m=0
+	last_step=0
+	bounds=0
+	
 	printf "\nChecking: $vnum\n"
 	while true; do
 		pg_nums=(`sed -ne "s/.*\/videos\/tmb\/\([0-9]\+\)\/.*/\1/p" $pg_file`)
-		if [ $vnum -le ${pg_nums[0]} -a $vnum -ge ${pg_nums[$(($npp-1))]} ]; then
-			echo in current page
+		[ ${#pg_nums[@]} -eq 0 ] && exit
+
+		if [ $vnum -le ${pg_nums[0]} -a $vnum -ge ${pg_nums[(($npp-1))]} ]; then
+			echo "Found vnum in current page"
 			title=$(sed -ne "/\/$vnum\//"'s/^.*title="\([^"]\+\)" .*/\1/p' $pg_file)
 			[ -z "$title" ] && title="Unknown"
-			printf "%d --> %s\n" $vnum "$title" >> $target
+			printf "%d --> %s\r\n" $vnum "$title" >> $target
 			break
-		elif [ $vnum -lt ${pg_nums[$(($npp-1))]} ]; then
-			pgn=`expr \( ${pg_nums[0]} - $vnum \) / $npp`
-			let pg_cur+=$pgn
+		elif [ $vnum -lt ${pg_nums[(($npp-1))]} ]; then
+			step_m=${pg_nums[0]}
 		elif [ $vnum -gt ${pg_nums[0]} ]; then
-			pgn=`expr \( $vnum - ${pg_nums[$(($npp-1))]} \) / $npp`
-			let pg_cur-=$pgn
+			step_m=${pg_nums[(($npp-1))]}
 		fi
+
+		echo last_step: $last_step
+		if [ $bounds -eq 0 ]; then
+			step=`expr \( $step_m - $vnum \) / $npp`
+			if [ $(( last_step + step )) -eq 0 ]; then
+				bounds=1
+				let step=last_step/-2
+			fi
+		else
+			let step/=-2
+		fi
+
+		if [ $step -eq 0 ]; then
+			printf "%d --> %s\r\n" $vnum "Unknown~" >> $target
+			break
+		fi
+
+		let pg_cur+=$step
+		last_step=$step
+
 		get_vpage $pg_cur
 	done
 done
