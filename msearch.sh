4b36dc08f422764ba5436cd077bae912778f6fbe
Barry Gu <jianzhong.gu@harman.com>
2014-12-15 14:57:03 +0800

    support no time (-t); use function to test file

--- msearch.sh
+++ msearch.sh
@@ -3,8 +3,11 @@
 #proxy="-e http_proxy=http://jiangu:Bag%400305@10.10.40.10:80"
 
 [ $# -lt 1 ] && exit
+
 list_mode=1
 down_flag=0
+no_time=0
+
 while [ $# -ge 2 ]; do
     if [ "$1" == "-l" ]; then
         list_mode=0
@@ -12,6 +15,8 @@ while [ $# -ge 2 ]; do
         down_flag=1
     elif [ "$1" == "-x" ]; then
         proxy="--no-proxy"
+    elif [ "$1" == "-t" ]; then
+        no_time=1
     else
         break;
     fi
@@ -25,32 +30,48 @@ query_result=result_$key.html
 keylist=list_$key.txt
 timelist=timl_$key.txt
 
-doit=0
-if [ -f $query_result ]; then
-    if [ `stat -c %s $query_result` -eq 0 ]; then
-        rm -f $query_result
-        doit=1
-    fi
-else
-    doit=1
-fi
-[ $doit -eq 1 ] && wget $proxy -O $query_result "$query_url$key"
+function OkFile()
+{
+    [ $# -ne 1 ] && return 0
 
-if [ ! -f $keylist -o `stat -c %s $keylist` -eq 0 ]; then
-    if [ -f $query_result -a `stat -c %s $query_result` -ne 0 ]; then
+    local fil=$1
+    [ -z "$fil" ] && return 0
+    [ ! -f $fil ] && return 0
+    [ `stat -c %s $fil` -eq 0 ] && return 0
+
+    return 1
+}
+
+OkFile $query_result
+[ $? -eq 0 ] && wget $proxy -O $query_result "$query_url$key"
+
+OkFile $keylist
+if [ $? -eq 0 ]; then
+    OkFile $query_result
+    if [ $? -eq 1 ]; then
         sed -ne 's/.*\/video\/\([0-9a-f]\+\)\/[^>]*><img.*\/tmb[0-9]*\/\([0-9]\+\)\/.*title="\([^"]\+\)".*/\1 \2 \3/p' $query_result > $keylist
     fi
 fi
 
-[ ! -f $keylist -o `stat -c %s $keylist` -eq 0 ] && exit
+OkFile $keylist
+[ $? -eq 0 ] && exit
 
-rm -f $timelist
-touch $timelist
-for id in `cut -f 1 -d\  $keylist`; do
-    sed -n -e "/\/video\/$id\//,/box_right/!d;s/<[^>]\+>//;s/[ \ t]\+//" -e '/[0-9][0-9]:[0-9][0-9]/p' $query_result >> $keylist.tim
-done
-paste $keylist $keylist.tim > $timelist
-rm -f $keylist.tim
+if [ $no_time -eq 0 ]; then
+    OkFile $timelist
+    if [ $? -eq 0 ]; then
+        rm -f $timelist
+        touch $timelist
+        for id in `cut -f 1 -d\  $keylist`; do
+            sed -n -e "/\/video\/$id\//,/box_right/!d;s/<[^>]\+>//;s/[ \ t]\+//" -e '/[0-9][0-9]:[0-9][0-9]/p' $query_result >> $keylist.tim
+        done
+        paste $keylist $keylist.tim > $timelist
+        rm -f $keylist.tim
+    fi
+    OkFile $timelist
+    [ $? -eq 0 ] && exit
+else
+    timelist=$keylist
+fi
 
 [ $list_mode -eq 1 ] && tac $timelist
 
