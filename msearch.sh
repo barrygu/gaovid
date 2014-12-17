a4903f47b774390e2860ac6d3693d731107f5726
Barry Gu <jianzhong.gu@harman.com>
2014-12-17 12:02:57 +0800

    use single line sed command to parse needed fields

--- msearch.sh
+++ msearch.sh
@@ -6,7 +6,6 @@
 
 list_mode=1
 down_flag=0
-no_time=0
 
 while [ $# -ge 2 ]; do
     if [ "$1" == "-l" ]; then
@@ -15,8 +14,6 @@ while [ $# -ge 2 ]; do
         down_flag=1
     elif [ "$1" == "-x" ]; then
         proxy="--no-proxy"
-    elif [ "$1" == "-t" ]; then
-        no_time=1
     else
         break;
     fi
@@ -28,7 +25,6 @@ ipaddr=199.195.197.140
 query_url="http://$ipaddr/search?search_type=videos&search_query="
 query_result=result_$key.html
 keylist=list_$key.txt
-timelist=timl_$key.txt
 
 function OkFile()
 {
@@ -49,31 +45,14 @@ OkFile $keylist
 if [ $? -eq 0 ]; then
     OkFile $query_result
     if [ $? -eq 1 ]; then
-        sed -ne 's/.*\/video\/\([0-9a-f]\+\)\/[^>]*><img.*\/tmb[0-9]*\/\([0-9]\+\)\/.*title="\([^"]\+\)".*/\1 \2 \3/p' $query_result > $keylist
+        sed -ne '/^ *<div class="video_box">/{n;s/.*<a [^>]*\/\([0-9a-f]\+\)\/.*<img[^>]*\/\([0-9]\+\)\/[^ ]* \+title="\([^"]\+\)".*/\1\n\2\n\3/;h;:a;n;/<div class="box_left">/!ba;:z;n;/[0-9:]\+/!bz;s/ *\([0-9:]\+\).*/ \1/;H;g;s/\(.\+\)\n\(.\+\)\n\(.\+\)\n\(.\+\)/\1\t\2  \t\4   \t\3/p}' $query_result > $keylist
     fi
 fi
 
 OkFile $keylist
 [ $? -eq 0 ] && exit
 
-if [ $no_time -eq 0 ]; then
-    OkFile $timelist
-    if [ $? -eq 0 ]; then
-        rm -f $timelist
-        touch $timelist
-        for id in `cut -f 1 -d\  $keylist`; do
-            sed -n -e "/\/video\/$id\//,/box_right/!d;s/<[^>]\+>//;s/[ \ t]\+//" -e '/[0-9][0-9]:[0-9][0-9]/p' $query_result >> $keylist.tim
-        done
-        paste $keylist $keylist.tim > $timelist
-        rm -f $keylist.tim
-    fi
-    OkFile $timelist
-    [ $? -eq 0 ] && exit
-else
-    timelist=$keylist
-fi
-
-[ $list_mode -eq 1 ] && tac $timelist
+[ $list_mode -eq 1 ] && tac $keylist
 
 if [ $down_flag -eq 1 ]; then
 	list=`cut -d' ' -f1 $keylist | tac`
