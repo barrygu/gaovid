1e755d14317f87f6f713cdb4a761bb9f83e0aeae
Barry Gu <jianzhong.gu@harman.com>
2017-01-30 11:54:25 +0800

    improve wide characters handle

--- 01mkpls.sh
+++ 01mkpls.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 
-find -name "*.mp4" | \
-  gawk '
-    BEGIN{
-      i=1
-    }{
-      sub(/^\.\//,"",$0); 
-      match($0, /^([0-9]+)[\.\-].*$/, aa); 
-      arr[aa[1]]=$0; 
-      brr[i]=aa[1]; 
-      i++
-    } END{
-      count=i
-      asort(brr);
-      print "[playlist]\nNumberOfEntries=" count > "01index.pls"
-      while ((getline < "00index.txt") > 0) {
-        map[$1]=$2
-        for (i=3;i<=NF;i++)
-          map[$1]=map[$1]$i
-      }
-      for(i=count-1;i>0;i--){
-        printf "File%d=%s\nTitle%d=%s\n", count-i, arr[brr[i]], count-i, map[arr[brr[i]]] >> "01index.pls"
-      }
-    }'
+lines=$(wc -l 00index.txt)
+
+printf "[playlist]\nNumberOfEntries=%d\n" > 01index.pls
+index=0
+while read -d $'\n' line; do
+    (( index++ ))
+    printf "File%d=%s\nTitle%d=%s\n" $index "${line%% *}" $index "${line#* }"
+done < 00index.txt >> 01index.pls
